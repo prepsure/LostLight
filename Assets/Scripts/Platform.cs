a35362f445fded64adf42ef62e6ed80413dd4bd6
Paige <33643911+presssssure@users.noreply.github.com>
2023-02-06 23:55:57 -0500

    made a LOT OF PROGRESS

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Transform _transform;
+    private BoxCollider _collider;
+
+    public Vector3 Position { get
+        {
+            return _transform.position;
+        } 
+    }
+
+
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _collider = GetComponent<BoxCollider>();
+    }
+
+    public static Platform[] GetAllPlatforms()
+    {
+        return FindObjectsOfType<Platform>();
+    }
+
+    public Vector3 GetInsetCharacterPosition(Transform characterTransform, Transform cameraTransform)
+    {
+        return Vector3.zero;
+    }
+
+    public void SetCollidable(bool shouldCollide)
+    {
+        _collider.enabled = shouldCollide;
+    }
+
+    public float GetTopYValue()
+    {
+        return -1000;
+    }
+
+    public bool IsPointInFrontOf(Vector3 point, Transform cameraTransform)
+    {
+        return true;
+    }
+
+    public bool IsPointBehind(Vector3 point, Transform cameraTransform)
+    {
+        return true;
+    }
+
+    public bool IsPointAbove(Vector3 point)
+    {
+        return true;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,34 +5,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private CharacterController _controller;
-    private float _yVelocity;
-
-    [SerializeField]
-    private float _gravity;
     [SerializeField]
     private float _playerSpeed;
     [SerializeField]
-    private float _jumpHeight;
+    private float _jumpPower;
 
-    private event Action OnJumpRequest;
-    private event Action<Vector3> OnMoveRequest;
-    private event Action OnGrappleRequest;
+    private float _characterSinkLeeway;
+
+    private Rigidbody _body;
+    private Collider _collider;
+    private Transform _transform;
+    private Camera _camera;
+    private Platform _currentPlatform;
 
     // Start is called before the first frame update
     void Start()
     {
-        _controller = GetComponent<CharacterController>();
-
-        OnMoveRequest += (Vector3 direction) =>
-        {
-            _controller.Move(_playerSpeed * Time.deltaTime * direction);
-        };
-
-        OnJumpRequest += () =>
-        {
-
-        };
+        _body = GetComponent<Rigidbody>();
+        _collider = GetComponent<MeshCollider>();
+        _transform = GetComponent<Transform>();
+        _camera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
@@ -40,12 +32,112 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        if(horizontalInput != 0)
+        SetMoveVector(horizontalInput);
+
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            Vector3 direction = new(horizontalInput, 0, 0);
-            OnMoveRequest.Invoke(direction);
+            TryToJump();
         }
 
-        _controller.Move(new Vector3(0, -_gravity, 0) * Time.deltaTime);
+        LimitYVelocity();
+        UpdatePlatform();
+
+        Platform plat = _currentPlatform;
+        if (plat)
+        {
+            /*_transform.SetPositionAndRotation(
+                plat.GetInsetCharacterPosition(
+                    _transform,
+                    _camera.GetComponent<Transform>()
+                ), 
+                _transform.rotation
+            );*/
+        }
     }
-}
+
+    bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, (float) (_collider.bounds.extents.y + 0.1));
+    }
+
+    void TryToJump()
+    {
+        if (!IsGrounded())
+        {
+            return;
+        }
+
+        _body.velocity = new Vector3(_body.velocity.x, _jumpPower, 0);
+    }
+
+    void SetMoveVector(float moveDir)
+    {
+        Vector3 potentialMove = new Vector3(moveDir * _playerSpeed, _body.velocity.y, 0);
+        _body.velocity = potentialMove;
+    }
+
+    void LimitYVelocity()
+    {
+        _body.velocity = new Vector3(_body.velocity.x, Math.Min(_jumpPower, _body.velocity.y), 0);
+    }
+    
+    Vector3[] GetFeetLocations()
+    {
+        // TODO
+        Vector3 corner1 = _collider.bounds.min;
+        Vector3 corner2 = new(_collider.bounds.max.x, corner1.y, corner1.z);
+        return new Vector3[]{ corner1, corner2};
+    }
+
+    void UpdatePlatform()
+    {
+        Platform[] allPlats = Platform.GetAllPlatforms();
+        Transform cameraTransform = _camera.GetComponent<Transform>();
+
+        _currentPlatform = null;
+        Platform tallestUsablePlat = null;
+        
+        foreach (Vector3 feetPos in GetFeetLocations())
+        {
+            foreach (Platform plat in allPlats)
+            {
+                // true by default, set to false if needed
+                plat.SetCollidable(true);
+
+                if (tallestUsablePlat && (plat.GetTopYValue() < tallestUsablePlat.GetTopYValue()))
+                { // is it taller than the one i already have?
+                    continue;
+                }
+
+                if (
+                    tallestUsablePlat &&
+                    plat.GetTopYValue() == tallestUsablePlat.GetTopYValue() &&
+                    !tallestUsablePlat.IsPointInFrontOf(plat.Position, cameraTransform) // if a platform is behind the current one
+                ) {
+                    continue;
+                }
+
+                //local feetPos = Character:GetFeetPos()
+
+                if (plat.GetTopYValue() > feetPos.y + _characterSinkLeeway)
+                { // is it under feet?
+                    Debug.Log("hi");
+                    plat.SetCollidable(false);
+                    continue;
+                }
+
+                if (!(
+                    plat.IsPointInFrontOf(feetPos, cameraTransform) ||
+                    plat.IsPointBehind(feetPos, cameraTransform) ||
+                    plat.IsPointAbove(feetPos)
+                ))
+                {
+                    continue;
+                }
+
+                tallestUsablePlat = plat;
+            }
+        }
+
+        _currentPlatform = tallestUsablePlat;
+    }
+ }
